be90784651079f40953de25308bd64fd3cb5c22e
xhechar <felkozdzonchez3@gmail.com>
2025-12-16 08:26:34 +0300

    add plus sign to total display in Total component

--- src/app/components/Total.tsx
+++ src/app/components/Total.tsx
@@ -5,7 +5,7 @@ type TotalProps = {
 export default function Total(props: TotalProps) {
   return (
     <div className="flex justify-center items-center w-[56px] h-[56px] bg-white border-[#E0E0E0] border-[3px] rounded-full">
-      <span className="body3">{props.total}</span>
+      <span className="body3">+{props.total}</span>
     </div>
   )
 }
